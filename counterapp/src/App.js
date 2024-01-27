4940fdba9b97a38ff393bab5bdbcdc9605ce2954
Pawan-Gautam11 <pawangautam11op@gmail.com>
2024-01-27 22:31:40 +0545

    Normal useful things for react

---
+++ counterapp/src/App.js
@@ -0,0 +1,16 @@
+import React,{useState} from "react";
+import style from "./style.css";
+
+const App =() =>{
+    const[Number,SetNumber] = useState(0);
+    return <div>
+        <h1>{Number}</h1> <hr></hr>
+        
+        <button onClick={() =>SetNumber(Number + 1)} className="button">Increment</button>
+        <button onClick={() =>SetNumber(Number -1) } className="button">Decrement</button>
+        <button onClick={() =>SetNumber(0)} className="button">Reset</button>
+    </div> 
+}
+
+
+export default App;
