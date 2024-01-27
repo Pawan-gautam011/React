d6a035cc93d8c7a03c909ecce953b8523f673ff4
Pawan-Gautam11 <pawangautam11op@gmail.com>
2024-01-27 14:29:47 +0545

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './style.css';
+
+function App() {
+  return <h1>Hello WOrld</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
