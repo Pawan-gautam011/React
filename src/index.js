9c5265b5025c03e5ba1635ba94f6131d10c125ad
Pawan-Gautam11 <pawangautam11op@gmail.com>
2024-01-27 15:30:19 +0545

    React first day code done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import './style.css';
+import App from "./App"; 
 
-function App() {
-  return <h1>Hello WOrld</h1>;
-}
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
