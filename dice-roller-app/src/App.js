e2a62031cdb995ef469f95ebc30d3052e8001d80
Pawan-Gautam11 <pawangautam11op@gmail.com>
2024-01-28 23:47:07 +0545

    Initial commit

---
+++ dice-roller-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './index.css';
+
+function App() {
+  return (
+    <div>
+     <img width={300} height={300} src={require('\src\assests/1.png')}></img>
+     
+
+    </div>
+  );
+}
+
+export default App;
