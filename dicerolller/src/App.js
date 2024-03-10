0c5beaaecfc8a16bb1be6638849bc95f5a585d16
Pawan Gautam <gautampawan4002@gmail.com>
2024-03-10 23:17:59 +0545

    \first commit after long at diceroller

---
+++ dicerolller/src/App.js
@@ -0,0 +1,13 @@
+import './index.css';
+
+function App() {
+  return (
+  <div>
+  <img width={300} height={250} src={require('./assets/1.png')} alt="example"></img>
+
+    <button ClassName='buttons'>Roll here</button>
+  </div>
+  );
+}
+
+export default App;
