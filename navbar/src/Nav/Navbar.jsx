a00d9382ac470863de9b01386159cf63f6fad404
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-04-04 19:36:17 +0545

    Practice of Navbar in React for the first time

---
+++ navbar/src/Nav/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Navbar.css'
+
+
+const Navbar = () => {
+  return (
+    <>
+    <header className='header'>
+        <div className='navbar'>
+            <h1>Pa<span>W</span>an</h1>
+<ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact</li>
+    <li>Services</li>
+</ul>
+        </div>
+
+    </header>
+
+    <p style={{color:'red',textAlign:'center',marginTop:'20%',fontSize:'40px'}}>Practice Page</p>
+  </>
+  )
+}
+
+export default Navbar
