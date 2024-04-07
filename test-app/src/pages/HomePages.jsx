4afe2830eaacfd77826310934e11a5c3eff60f86
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-04-07 23:53:31 +0545

    Learn about react helmet

--- test-app/src/pages/HomePages.jsx
+++ test-app/src/pages/HomePages.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Carousel from '../components/Carousel'
 import { Card } from '../components/Card'
+import {Helmet} from 'react-helmet'
+
 
 
 const HomePages = () => {
   return (
     <>
+  <Helmet>
+    <title>Ecommerce-site</title>
+  </Helmet>
     <Carousel/>
     <Card/>
 
