7489331a72ebf0d93515a8d5a6b230d1d9dd3a12
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-05-10 22:31:28 +0545

    A form by using react but not complete for now

---
+++ Login_Form/Components/LoginPage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+   <form>
+    <div className='mb-2'>
+        <h1>Login</h1>
+        <div className='form-control'>
+          <label>Username</label>
+          <input type='text' placeholder='Enter Username' />
+        </div>
+        <div className='form-control'>
+          <label>Password</label>
+          <input type='password' placeholder='Enter Password' />
+        </div>
+        <button className='btn btn-primary btn-block'>Login</button>
+        <button className='btn btn-dark btn-block'>Register</button>
+
+    </div>
+   </form>
+  )
+}
+
+export default LoginPage
---
+++ Login_Form/src/App.jsx
@@ -0,0 +1,12 @@
+import LoginPage from '../Components/LoginPage'
+import './App.css'
+
+function App() {
+  return (
+    <>
+<LoginPage />
+    </>
+  )
+}
+
+export default App
