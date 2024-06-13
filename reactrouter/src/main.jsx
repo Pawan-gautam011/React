0ced577e9171eafbc1cf694f380e1981aab18770
Pawan-gautam011 <pawangautam11op@gmail.com>
2024-06-13 14:21:36 +0545

    Clearing the concept of router dom throught this multi components project

---
+++ reactrouter/src/main.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import './index.css'
+import {RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Footer from './components/Footer/Footer'
+import Contact from './components/Contact/Contact'
+import User from './components/User/User'
+import Github from './components/Github/Github'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {path:"about",
+        element:<About/>
+      },
+      {path:"footer",
+        element:<Footer/>
+      },
+      {path:"contact",
+        element:<Contact/>
+      },
+      {
+        path:"user/:userid",
+        element:<User/>
+      },
+      {path:"github",
+        element:<Github/>
+      }
+
+    ]
+  }
+])
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
